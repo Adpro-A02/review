1a6553f21300d1a2eba7e9ec7381c8632a6bcea1
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-05-12 15:50:20 +0700

    [GREEN] Add ReviewRepository file

---
+++ src/main/java/id/ac/ui/cs/advprog/review/repository/ReviewRepository.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.review.repository;
+
+import id.ac.ui.cs.advprog.review.model.ReviewModel;
+import id.ac.ui.cs.advprog.review.model.ReviewStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewModel, UUID> {
+    List<ReviewModel> findAll();
+
+    @Override
+    <S extends ReviewModel> S save(S entity);
+
+    void deleteById(UUID id);
+
+    List<ReviewModel> findAllByStatus(ReviewStatus status);
+}
